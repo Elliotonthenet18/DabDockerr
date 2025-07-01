8e64696ce89574dff5fa108d4ecb9e311d021814
Elliotonthenet18 <alexeugeneprice@gmail.com>
2025-07-01 16:17:09 -0500

    Update chrome-launcher.sh

--- chrome-launcher.sh
+++ chrome-launcher.sh
@@ -35,6 +35,7 @@ export DISPLAY=:1
     --disable-ipc-flooding-protection \
     --kiosk \
     --app="https://dab.yeet.su/" &
+    --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36" \
 
 # Keep the script running
 wait
