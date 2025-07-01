1a7fc7e2531998535a1635420076fce0f3f87c40
Elliotonthenet18 <alexeugeneprice@gmail.com>
2025-07-01 16:21:47 -0500

    Update chrome-launcher.sh

--- chrome-launcher.sh
+++ chrome-launcher.sh
@@ -34,8 +34,9 @@ export DISPLAY=:1
     --disable-features=TranslateUI \
     --disable-ipc-flooding-protection \
     --kiosk \
-    --app="https://dab.yeet.su/" &
-    --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36" \
+    --app="https://dab.yeet.su/" \
+    --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
 
 # Keep the script running
 wait
+
